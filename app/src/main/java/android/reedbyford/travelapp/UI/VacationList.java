a56753419e175fcf83d45f4d9efd01a846bafe76
reedbbyford <reedbyford@gmail.com>
2023-03-21 22:18:57 -0500

    Initial commit

---
+++ app/src/main/java/android/reedbyford/travelapp/UI/VacationList.java
@@ -0,0 +1,15 @@
+package android.reedbyford.travelapp.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.reedbyford.travelapp.R;
+
+public class VacationList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vacation_list);
+    }
+}
